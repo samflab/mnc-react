ec659a8b11a7dfc26dd543d10d4afd44c15cd1b1
Masudha Meher <kasturimeh@gmail.com>
2022-03-19 14:00:09 +0530

    refactor: wishlist on home page

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -1,37 +1,83 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Product } from "../ProductList/Product";
+import { useWishlist } from "../../context/wishlist-context";
+
 
 export const Trending = () => {
-  const [product, setProduct] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const { wishlistState, wishlistDispatch } = useWishlist();
 
-  const getData = async () => {
+  const getTrendingProducts = async () => {
     const response = await axios.get("/api/products");
     const data = response.data.products;
     const newData = [...data].filter((i) => i.tag === "trending");
-    setProduct(newData);
+    setTrending(newData);
     return newData;
   };
+
   useEffect(() => {
-    getData();
+    getTrendingProducts();
   }, []);
+
   return (
     <div>
-      <h2 class="trending-header">--- Trending ---</h2>
+      <h2 class="trending-header">Trending</h2>
       <p class="trending-sub">Top reads this week</p>
       <div class="trending">
-        {product.map((i) => {
-          const discount = Math.ceil((i.price / i.originalPrice) * 100);
+        {trending.map((product) => {
           return (
-            <Product
-              title={i.title}
-              discount={discount}
-              author={i.author}
-              img={i.img}
-              rating={i.rating}
-              price={i.price}
-              originalPrice={i.originalPrice}
-            />
+            <div class="product" key={product.id}>
+              <div class="product-img">
+                <img
+                  src={product.img}
+                  alt={product.title}
+                  class="img"
+                />
+                { wishlistState.find(wishlistItem => wishlistItem._id === product._id) ? (
+                  <span key={product._id}
+                    className=""
+                    onClick={() =>
+                      wishlistDispatch({
+                        type: "REMOVE_FROM_WISHLIST",
+                        payload: product,
+                      })
+                    }
+                  >
+                    <i className="far fa-times-circle close bookmark"></i>
+                  </span>
+                ) : (
+                  <span key={product._id}
+                    class=""
+                    onClick={() =>
+                      wishlistDispatch({
+                        type: "ADD_TO_WISHLIST",
+                        payload: product,
+                      })
+                    }
+                  >
+                    <i class="far fa-bookmark bookmark"></i>
+                  </span>
+                )}
+              </div>
+              <div class="card-body">
+                <div class="product-name-container">
+                  <span class="product-name">{product.title}</span>
+
+                  <span class="rating" role="img">
+                    {product.rating}/5⭐
+                  </span>
+                </div>
+                <span class="author">by {product.author}</span>
+                <div style={{ marginBlock: "1rem" }}>
+                  <span class="price-tag">₹{product.price}</span>
+                  <del class="price-tag original">
+                    ₹{product.originalPrice}
+                  </del>
+                  <span class="price-tag discount">{product.discount}% Off</span>
+                </div>
+                <button class="add-to-cart">Add to Cart</button>
+              </div>
+            </div>
           );
         })}
       </div>
