c2ea6ba39e64ba72e70de2bb8825683b604ffdcf
Masudha Meher <kasturimeh@gmail.com>
2022-03-20 13:59:08 +0530

    fix: removed unwated files

--- src/util/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getCategory = async () =>{
-    const response = await axios.get("/api/categories");
-    const data = response.data.categories;
-    return data;
-}
