41d0238bdf47cb15db43ec22a9a28c3357145e83
Masudha Meher <kasturimeh@gmail.com>
2022-03-23 19:38:11 +0530

    refactor: barrel import

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { makeServer } from "./server";
-import { FilterProvider } from "./context/filter-context";
-import { WishlistProvider } from "./context/wishlist-context";
-import { CartProvider } from "./context/cart-context";
-import { AuthProvider } from "./context/auth-context";
+import {AuthProvider, CartProvider, FilterProvider, WishlistProvider} from "./context"
 
 // Call make Server
 makeServer();
